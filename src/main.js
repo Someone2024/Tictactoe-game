aeee480ad89198162830684ace06b9955ce160d3
Someone2024 <bassilsantiago@gmail.com>
2023-05-18 14:39:26 -0500

    Winner verification and restar button added. Now START working on the css!

--- src/main.js
+++ src/main.js
@@ -7,17 +7,49 @@ const GameBoard = (() => {
 
 const Player = (mark) => {
   const marker = mark;
-
   return { marker };
 };
 
 const player1 = Player("X");
 const player2 = Player("O");
 let currentTurn = true;
+let winner = "";
+
+function checkWinner() {
+  const winningConditions = [
+    [0, 1, 2], // Top row
+    [3, 4, 5], // Middle row
+    [6, 7, 8], // Bottom row
+    [0, 3, 6], // Left column
+    [1, 4, 7], // Middle column
+    [2, 5, 8], // Right column
+    [0, 4, 8], // Diagonal from top-left
+    [2, 4, 6], // Diagonal from top-right
+  ];
+
+  for (let condition of winningConditions) {
+    const [a, b, c] = condition;
+    if (
+      GameBoard.Board[a] !== "" &&
+      GameBoard.Board[a] === GameBoard.Board[b] &&
+      GameBoard.Board[b] === GameBoard.Board[c]
+    ) {
+      winner = GameBoard.Board[a];
+      break;
+    }
+  }
+}
 
 function displayBoard() {
+  const winnerAd = document.createElement("p");
+  const restar = document.createElement("button");
+  restar.textContent = "Restar"
+  document.body.appendChild(winnerAd);
+  document.body.appendChild(restar);
+
   for (let i = 0; i < GameBoard.Board.length; i++) {
     const markDiv = document.createElement("button");
+
     markDiv.textContent = GameBoard.Board[i];
     markDiv.classList.add("mark");
     markDiv.addEventListener("click", () => {
@@ -25,27 +57,30 @@ function displayBoard() {
         markDiv.textContent = player1.marker;
         GameBoard.Board[i] = player1.marker;
         currentTurn = false;
-        
-      }else if (markDiv.textContent === "" && !currentTurn){
-        markDiv.textContent = player2.marker
+      } else if (markDiv.textContent === "" && !currentTurn) {
+        markDiv.textContent = player2.marker;
         GameBoard.Board[i] = player2.marker;
-        currentTurn = true
+        currentTurn = true;
       }
+
+      console.log(GameBoard.Board);
+      checkWinner();
+
+      if (winner === "X") {
+        winnerAd.textContent = "Player X wins";
+      } else if (winner === "O") {
+        winnerAd.textContent = "Player O wins";
+      }
+    });
+
+    restar.addEventListener("click", ()=>{
+      GameBoard.Board = ["", "", "", "", "", "", "", "", ""];
       console.log(GameBoard.Board)
-      checkWinner()
+      markDiv.textContent = GameBoard.Board[i];
+      currentTurn = true;
     });
     board.append(markDiv);
   }
 }
-function checkWinner(){
-  for (let i = 0; i < GameBoard.Board.length; i++) {
-    if(GameBoard.Board[i] + 2 === player1.marker){
-      console.log("el pepe");
-    }    
-  }
-} 
+
 displayBoard();
-
-
-//GO to https://www.theodinproject.com/lessons/node-path-javascript-tic-tac-toe
-//to find out whats left to do
